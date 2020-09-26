f9cc08e585e66e11f4ba4e9d865750fcd251a9b6
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-26 16:26:30 -0700

    CRA + packages installed

---
+++ pearch-fe-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/App.css';
+import { Sidebar } from './components'
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <h1>Hello</h1>
+      <Sidebar />
+
+      {/*  Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ pearch-fe-app/src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            {/* Twitter icon */}
+
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+
+            {/* Button -> Draft Tweet */}
+        </div>
+    )
+}
+
+export default Sidebar
+
+
