fe7ac02fb0a20b140691845307ffda74539ba44e
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-26 17:26:56 -0700

    sidebar styled and complete

--- pearch-fe-app/src/components/Sidebar.js
+++ pearch-fe-app/src/components/Sidebar.js
@@ -8,7 +8,7 @@ import MailOutlineIcon from "@material-ui/icons/MailOutline"
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
 import ListAltIcon from '@material-ui/icons/ListAlt'
 import PermIdentityIcon from '@material-ui/icons/PermIdentity'
-import MoreHoriz from '@material-ui/icons/MoreHoriz'
+import MoreHorizOutlined from '@material-ui/icons/MoreHorizOutlined'
 import { Button } from '@material-ui/core'
 
 import SidebarOption from './SidebarOption'
@@ -16,7 +16,7 @@ import SidebarOption from './SidebarOption'
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <TwitterIcon />
+            <TwitterIcon className='sidebar__twitterIcon' />
 
             <SidebarOption active Icon={HomeIcon} text='Home' />
             <SidebarOption Icon={SearchIcon} text="Explore"/>
@@ -25,7 +25,7 @@ function Sidebar() {
             <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/>
             <SidebarOption Icon={ListAltIcon} text="Lists"/>
             <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
-            <SidebarOption Icon={MoreHoriz} text="Options"/>
+            <SidebarOption Icon={MoreHorizOutlined} text="Options"/>
 
 
             <Button variant='outlined' className='sidebar__tweet'>Draft Tweet</Button>
