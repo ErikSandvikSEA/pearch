b58e072970cc6a77fc0e92b3f0e7ea34c2f745c0
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-26 22:04:10 -0700

    mellin

--- pearch-fe-app/src/App.js
+++ pearch-fe-app/src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './styles/App.css';
 import { Sidebar, Feed, Widgets } from './components'
+import axios from 'axios'
+import { mellin } from './secrets/secrets'
 
+const baseUrl = 'https://api.twitter.com/'
+const validToken = 'AAAAAAAAAAAAAAAAAAAAANdXIAEAAAAANDnUKIWbuosSmrf7rcbVb09GElU%3Dv17iS8cESY6DDtBbr4W0ek5byXSgs6P4pPCjUtvEnCf430Jtml'
+
+const config = {
+  headers: {
+    'Authorization': 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAANdXIAEAAAAANDnUKIWbuosSmrf7rcbVb09GElU%3Dv17iS8cESY6DDtBbr4W0ek5byXSgs6P4pPCjUtvEnCf430Jtml'
+  }
+}
+
+const getData = () => {
+  axios
+    .get()
+}
 
 function App() {
   return (
