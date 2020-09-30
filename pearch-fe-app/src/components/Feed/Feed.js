55b021d8c1f96e872dfffdf37d0e5b4374607e35
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-09-29 22:03:19 -0700

    get request rendering posts upon search - horizontal scroll fixed

--- pearch-fe-app/src/components/Feed/Feed.js
+++ pearch-fe-app/src/components/Feed/Feed.js
@@ -16,7 +16,6 @@ function Feed(props) {
 
     return (
         <div className='feed'>
-            {/* Header */}
             <div className="feed__header">
                 <h2>Home</h2>
             </div>
@@ -32,18 +31,6 @@ function Feed(props) {
                     // image={post.image}
                 />
             ))}
-            {/* <Post 
-                displayName="Archimedes the Highly-Educated Owl"
-                username="@who_what_what"
-                verified={true}
-                text="YOOO it's working"
-                avatar="https://vignette.wikia.nocookie.net/disney/images/4/43/Archimedes.png/revision/latest?cb=20140301213627"
-                image="https://png.pngtree.com/png-clipart/20190629/original/pngtree-cute-owl-cartoon-set-png-image_4100837.jpg" 
-            />
-            <Post />
-            <Post />
-            <Post />
-            <Post /> */}
         </div>
     )
 }
